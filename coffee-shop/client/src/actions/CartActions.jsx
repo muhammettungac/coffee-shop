538141e04c472d8770fc57e463926f7d80af6d8a
muhammedtungac <muhammedtungac@gmail.com>
2023-04-03 01:11:21 +0300

    update
    Updated for every coffee's quantity design for CartPage

--- coffee-shop/client/src/actions/CartActions.jsx
+++ coffee-shop/client/src/actions/CartActions.jsx
@@ -5,13 +5,17 @@ export const addToCartAction =
       type: items.type,
       subType: items.subType,
       size: ozellik,
+      sizeKey: ozellikKey,
       price: items.price[ozellikKey],
       quantity: miktar,
       description: items.description,
       picture: items.picture,
     };
-    if (miktar < 0) {
-    } else {
+    if (miktar < 1) {
+      cartItem.quantity = 1;
+    }
+
+    if (miktar > 0) {
       dispatch({ type: "ADD_TO_CART", payload: cartItem });
 
       const cartItems = getState().addToCartReducer.cartItems;
--- coffee-shop/client/src/pages/CartPage.jsx
+++ coffee-shop/client/src/pages/CartPage.jsx
@@ -1,35 +1,81 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import addToCartReducer from "../reducers/CartReducers";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCartAction } from "../actions/CartActions";
 
 function CartPage() {
   const cartState = useSelector((state) => state.addToCartReducer);
+  const dispatch = useDispatch();
 
   const { cartItems } = cartState;
 
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
     <div className="container">
-      {cartItems.map((coffees) => (
-        <div className="row shadow border border-success m-2 p-0 rounded-5">
-          <div className="col mx-auto p-1 ">
-            <img
-              className="rounded-4"
-              src={coffees.picture}
-              alt=""
-              style={{ width: "200px" }}
-            />
-          </div>
-          <div className="col my-auto ">
-            <p className="text-success fw-bold">{coffees.title} </p>
-          </div>
-          <div className="col my-auto">
-            <p className="text-success fw-semibold">{coffees.size}</p>
-          </div>
-          <div className="col my-auto">
-            <p className="text-success fw-normal">{coffees.description}</p>
-          </div>
+      {cartItems.length == 0 ? (
+        <div className="text-center text-success">
+          Sepette Ürün Bulunmamaktadır.
         </div>
-      ))}
+      ) : (
+        cartItems.map((coffees) => (
+          <div className="row shadow border border-success m-2 p-0 rounded-5">
+            <div className="col mx-auto p-1 ">
+              <img
+                className="rounded-4"
+                src={coffees.picture}
+                alt=""
+                style={{ width: "200px" }}
+              />
+            </div>
+            <div className="col my-auto ">
+              <p className="text-success fw-bold">{coffees.title} </p>
+            </div>
+            <div className="col my-auto">
+              <p className="text-success fw-semibold">{coffees.size}</p>
+            </div>
+            <div className="col my-auto">
+              <p className="text-success fw-normal">{coffees.description}</p>
+            </div>
+            <div className="col my-auto">
+              <p className="text-success fw-normal">
+                {coffees.price * coffees.quantity}
+              </p>
+            </div>
+
+            <div className="col my-auto">
+              <div className="row">
+                <i
+                  class="fa-solid fa-circle-plus text-success"
+                  onClick={() =>
+                    dispatch(
+                      addToCartAction(
+                        coffees,
+                        Number(coffees.quantity) + 1,
+                        coffees.size,
+                        coffees.sizeKey
+                      )
+                    )
+                  }
+                ></i>
+                <p className="text-success fw-normal">{coffees.quantity}</p>
+                <i
+                  class="fa-solid fa-circle-minus text-success"
+                  onClick={() =>
+                    dispatch(
+                      addToCartAction(
+                        coffees,
+                        Number(coffees.quantity) - 1,
+                        coffees.size,
+                        coffees.sizeKey
+                      )
+                    )
+                  }
+                ></i>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
