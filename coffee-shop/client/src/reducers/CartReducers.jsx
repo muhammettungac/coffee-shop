c58991e95e990bfab8a55ee3d185276dc9850521
muhammedtungac <muhammedtungac@gmail.com>
2023-04-04 00:14:13 +0300

    update
    CartPage update

--- coffee-shop/client/src/reducers/CartReducers.jsx
+++ coffee-shop/client/src/reducers/CartReducers.jsx
@@ -1,11 +1,26 @@
 export const addToCartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return {
-        success: true,
-        ...state,
-        cartItems: [...state.cartItems, action.payload],
-      };
+      var isTrue = state.cartItems.find(
+        (item) => item._id == action.payload._id
+      );
+
+      if (isTrue) {
+        return {
+          success: true,
+          ...state,
+          cartItems: state.cartItems.map((data) =>
+            data._id === action.payload._id ? action.payload : data
+          ),
+        };
+      } else {
+        return {
+          success: true,
+          ...state,
+          cartItems: [...state.cartItems, action.payload],
+        };
+      }
+
     default:
       return state;
   }
